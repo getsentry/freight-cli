c10d633eec2da6b95500bee0b8f29cf60d525bf5
David Cramer <dcramer@gmail.com>
2015-02-14 13:35:20 -0800

    first commit

---
+++ ds_cli.py
@@ -0,0 +1,55 @@
+from __future__ import absolute_import, unicode_literals
+
+import click
+import requests
+
+
+import os
+import click
+
+
+class Api(object):
+    def __init__(self, base_url, api_key, user, debug=False):
+        self.base_url = base_url.rstrip('/')
+        self.api_key = api_key
+        self.user = user
+        self.debug = debug
+        self.session = self.build_session()
+
+    def build_session(self):
+        session = requests.Session()
+        s.headers.update({'Authorization': 'Key {}'.format(self.api_key)})
+        return session
+
+    delete = lambda url, *a, **k: self.session.delete(self.base_url + '/' + url, *a, **k)
+    get = lambda url, *a, **k: self.session.get(self.base_url + '/' + url, *a, **k)
+    post = lambda url, *a, **k: self.session.post(self.base_url + '/' + url, *a, **k)
+    put = lambda url, *a, **k: self.session.put(self.base_url + '/' + url, *a, **k)
+
+
+@click.group()
+@click.option('--api-key', required=True, envvar='DS_API_KEY')
+@click.option('--base-url', required=True, envvar='DS_URL')
+@click.option('--user', required=True, envvar='DS_USER')
+@click.option('--debug/--no-debug', default=False)
+@click.pass_context
+def cli(ctx, base_url, api_key, user, debug):
+    ctx.obj = Api(base_url, api_key, user, debug)
+
+
+@cli.command()
+@click.argument('app', required=True)
+@click.argument('env', default='production')
+@click.argument('ref', default='master')
+def deploy(api, app, env, ref):
+    api.post('/tasks/', {
+        'app': app,
+        'env': env,
+        'ref': ref,
+        'user': config.user
+    })
+
+
+
+if __name__ == '__main__':
+    cli()
---
+++ setup.py
@@ -0,0 +1,70 @@
+#!/usr/bin/env python
+"""
+ds-cli
+======
+
+:copyright: (c) 2015 GetSentry LLC
+:license: Apache 2.0, see LICENSE for more details.
+"""
+from __future__ import absolute_import, unicode_literals
+
+import os.path
+
+from setuptools import setup, find_packages
+
+
+# Hack to prevent stupid "TypeError: 'NoneType' object is not callable" error
+# in multiprocessing/util.py _exit_function when running `python
+# setup.py test` (see
+# http://www.eby-sarna.com/pipermail/peak/2010-May/003357.html)
+for m in ('multiprocessing', 'billiard'):
+    try:
+        __import__(m)
+    except ImportError:
+        pass
+
+ROOT = os.path.realpath(os.path.join(os.path.dirname(__file__)))
+
+tests_require = [
+    'flake8>=2.1.0,<2.2.0',
+    'mock>=1.0.1,<1.1.0',
+    'pytest>=2.5.0,<2.6.0',
+    'pytest-cov>=1.6,<1.7',
+    'pytest-timeout>=0.3,<0.4',
+    'pytest-xdist>=1.9,<1.10',
+    'responses>=0.3.0,<0.4.0',
+]
+
+
+install_requires = [
+    'click>=3.3.0,<3.4.0',
+    'requests>=2.5.1,<2.6.0',
+]
+
+setup(
+    name='ds-cli',
+    version='0.0.0',
+    author='David Cramer',
+    author_email='dcramer@gmail.com',
+    url='https://github.com/getsentry/ds-cli',
+    description='A client for DS',
+    long_description=open('README.md').read(),
+    packages=find_packages(exclude=['tests']),
+    zip_safe=False,
+    install_requires=install_requires,
+    extras_require={
+        'test': tests_require,
+    },
+    entry_points='''
+        [console_scripts]
+        ds-cli=ds_cli:cli
+    ''',
+    license='Apache 2.0',
+    include_package_data=True,
+    classifiers=[
+        'Intended Audience :: Developers',
+        'Intended Audience :: System Administrators',
+        'Operating System :: OS Independent',
+        'Topic :: Software Development'
+    ],
+)
