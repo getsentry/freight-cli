0715e510ee1907bf1c16ad0e379fb0e23d39351d
Armin Ronacher <armin.ronacher@active-4.com>
2015-02-14 22:49:00 +0100

    Fixed up stuff.

--- ds_cli.py
+++ ds_cli.py
@@ -1,52 +1,66 @@
-from __future__ import absolute_import, unicode_literals
+from __future__ import absolute_import
 
 import click
 import requests
 
 
-import os
-import click
-
-
 class Api(object):
-    def __init__(self, base_url, api_key, user, debug=False):
-        self.base_url = base_url.rstrip('/')
-        self.api_key = api_key
-        self.user = user
-        self.debug = debug
-        self.session = self.build_session()
 
-    def build_session(self):
+    def __init__(self):
+        self.base_url = None
+        self.api_key = None
+        self.user = None
+        self.debug = False
+        self._session = None
+
+    @property
+    def session(self):
+        if self._session is not None:
+            return self._session
         session = requests.Session()
         session.headers.update({'Authorization': 'Key {}'.format(self.api_key)})
+        self._session = session
         return session
 
-    delete = lambda url, *a, **k: self.session.delete(self.base_url + '/' + url, *a, **k)
-    get = lambda url, *a, **k: self.session.get(self.base_url + '/' + url, *a, **k)
-    post = lambda url, *a, **k: self.session.post(self.base_url + '/' + url, *a, **k)
-    put = lambda url, *a, **k: self.session.put(self.base_url + '/' + url, *a, **k)
+    delete = lambda self, url, *a, **k: \
+        self.session.delete(self.base_url + '/' + url, *a, **k)
+    get = lambda self, url, *a, **k: \
+        self.session.get(self.base_url + '/' + url, *a, **k)
+    post = lambda self, url, *a, **k: \
+        self.session.post(self.base_url + '/' + url, *a, **k)
+    put = lambda self, url, *a, **k: \
+        self.session.put(self.base_url + '/' + url, *a, **k)
 
 
-@click.group()
-@click.option('--api-key', required=True, envvar='DS_API_KEY')
-@click.option('--base-url', required=True, envvar='DS_BASE_URL')
-@click.option('--user', required=True, envvar='DS_USER')
+pass_api = click.make_pass_decorator(Api, ensure=True)
+
+
+@click.group(context_settings={
+    'auto_envvar_prefix': 'DS',
+})
+@click.option('--api-key', required=True)
+@click.option('--base-url', required=True)
+@click.option('--user', required=True)
 @click.option('--debug/--no-debug', default=False)
-@click.pass_context
-def cli(ctx, base_url, api_key, user, debug):
-    ctx.obj = Api(base_url, api_key, user, debug)
+@pass_api
+def cli(api, base_url, api_key, user, debug):
+    api.base_url = base_url
+    api.api_key = api_key
+    api.user = user
+    api.debug = debug
 
 
 @cli.command()
 @click.argument('app', required=True)
 @click.argument('env', default='production')
 @click.argument('ref', default='master')
+@pass_api
 def deploy(api, app, env, ref):
     api.post('/tasks/', {
         'app': app,
         'env': env,
         'ref': ref,
-        'user': config.user
+        'user': api.user
     })
 
 
