63ae813d5ef6696bf78dfbfec2855bba3527b6af
David Cramer <dcramer@gmail.com>
2015-02-14 13:54:54 -0800

    Basic working version of error handling + output

--- ds_cli.py
+++ ds_cli.py
@@ -3,9 +3,17 @@ from __future__ import absolute_import
 import click
 import requests
 
+from functools import partial
+
+class ApiError(Exception):
+    def __init__(self, code, error=None, error_name=None):
+        self.code = code
+        self.error = error
+        self.error_name = error_name
+        super(ApiError, self).__init__("{}: {}".format(self.code, self.error))
+
 
 class Api(object):
-
     def __init__(self):
         self.base_url = None
         self.api_key = None
@@ -22,14 +30,29 @@ class Api(object):
         self._session = session
         return session
 
-    delete = lambda self, url, *a, **k: \
-        self.session.delete(self.base_url + '/' + url, *a, **k)
-    get = lambda self, url, *a, **k: \
-        self.session.get(self.base_url + '/' + url, *a, **k)
-    post = lambda self, url, *a, **k: \
-        self.session.post(self.base_url + '/' + url, *a, **k)
-    put = lambda self, url, *a, **k: \
-        self.session.put(self.base_url + '/' + url, *a, **k)
+    def request(self, method, path, *args, **kwargs):
+        full_url = self.base_url + path
+        resp = getattr(self.session, method.lower())(full_url, *args, **kwargs)
+        content_type = resp.headers['Content-Type']
+        if content_type != 'application/json':
+            raise ApiError(
+                code=resp.status_code,
+                error='Invalid content type: {}'.format(content_type),
+            )
+
+        data = resp.json()
+        if 200 >= resp.status_code < 300:
+            return data
+        raise ApiError(
+            code=resp.status_code,
+            error=data['error'],
+            error_name=data.get('error_name'),
+        )
+
+    delete = lambda s, *a, **k: s.request('DELETE', *a, **k)
+    get = lambda s, *a, **k: s.request('GET', *a, **k)
+    post = lambda s, *a, **k: s.request('POST', *a, **k)
+    put = lambda s, *a, **k: s.request('PUT', *a, **k)
 
 
 pass_api = click.make_pass_decorator(Api, ensure=True)
@@ -44,7 +67,7 @@ pass_api = click.make_pass_decorator(Api, ensure=True)
 @click.option('--debug/--no-debug', default=False)
 @pass_api
 def cli(api, base_url, api_key, user, debug):
-    api.base_url = base_url
+    api.base_url = base_url.rstrip('/')
     api.api_key = api_key
     api.user = user
     api.debug = debug
@@ -56,12 +79,14 @@ def cli(api, base_url, api_key, user, debug):
 @click.argument('ref', default='master')
 @pass_api
 def deploy(api, app, env, ref):
-    api.post('/tasks/', {
+    response = api.post('/tasks/', {
         'app': app,
         'env': env,
         'ref': ref,
         'user': api.user
     })
+    data = response.json()
+    print 'Created new Task with ID = {}'.format(data['id'])
 
 
 if __name__ == '__main__':
