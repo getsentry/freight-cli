84b507455696b3af8fd98cf4553fc1c3b99c4943
Matt Robenolt <matt@ydekproductions.com>
2019-09-12 15:18:50 -0700

    convert to python3 and replace request with urllib3

--- freight_cli.py
+++ freight_cli.py
@@ -1,16 +1,15 @@
-from __future__ import absolute_import, print_function
-
-import click
-import re
-import requests
-import sys
 import json
-
+import re
+import sys
 from collections import namedtuple
 from time import sleep
+from urllib.parse import urlsplit
 
+import certifi
+import click
+from urllib3.connectionpool import connection_from_url
 
-Task = namedtuple('Task', ['app', 'env', 'number'])
+Task = namedtuple("Task", ["app", "env", "number"])
 
 
 class ApiError(Exception):
@@ -18,14 +17,15 @@ class ApiError(Exception):
         self.code = code
         self.error = error
         self.error_name = error_name
-        super(ApiError, self).__init__("{}: {}".format(self.code, self.error))
+        super().__init__(f"{self.code}: {self.error}")
 
 
 class Api(object):
-    _task_id_re = re.compile(r'^([^/]+)/([^#]+)#(\d+)$')
+    _task_id_re = re.compile(r"^([^/]+)/([^#]+)#(\d+)$")
 
     def __init__(self):
         self.base_url = None
+        self.path = None
         self.api_key = None
         self.user = None
         self.debug = False
@@ -35,148 +35,160 @@ class Api(object):
     def session(self):
         if self._session is not None:
             return self._session
-        session = requests.Session()
-        session.headers.update({
-            'Accept': 'application/json',
-            'User-Agent': 'freight-cli',
-            'Authorization': 'Key {}'.format(self.api_key),
-        })
+        session = connection_from_url(self.base_url, ca_certs=certifi.where())
+        session.headers.update(
+            {
+                "Accept": "application/json",
+                "User-Agent": "freight-cli",
+                "Authorization": f"Key {self.api_key}",
+            }
+        )
         self._session = session
         return session
 
     def parse_task_id(self, task_id):
         match = self._task_id_re.match(task_id)
         if not match:
-            raise ValueError('Task ID must be in format <app>/<env>#<number>')
+            raise ValueError("Task ID must be in format <app>/<env>#<number>")
         return Task(app=match.group(1), env=match.group(2), number=match.group(3))
 
-    def request(self, method, path, json=None, *args, **kwargs):
-        full_url = self.base_url + path
+    def request(self, method, path, body=None, *args, **kwargs):
+        full_path = self.path + path
 
-        if json:
-            assert method != 'GET'
-            kwargs['json'] = json
+        method = method.upper()
+        if body:
+            assert method != "GET"
+            kwargs["body"] = json.dumps(body)
             # TODO(dcramer): flask errors out right now if we send an empty
             # JSON body
-            kwargs['headers'] = {'Content-Type': 'application/json'}
+            kwargs["headers"] = {
+                **self.session.headers,
+                **{"Content-Type": "application/json"},
+            }
 
-        resp = getattr(self.session, method.lower())(full_url, *args, **kwargs)
+        resp = self.session.urlopen(method, full_path, *args, **kwargs)
 
-        content_type = resp.headers['Content-Type']
-        if content_type != 'application/json':
+        content_type = resp.headers["Content-Type"]
+        if content_type != "application/json":
             raise ApiError(
-                code=resp.status_code,
-                error='Invalid content type: {}'.format(content_type),
+                code=resp.status, error=f"Invalid content type: {content_type}"
             )
 
-        data = resp.json()
+        data = json.loads(resp.data)
 
-        if 200 <= resp.status_code < 300:
+        if 200 <= resp.status < 300:
             return data
 
         raise ApiError(
-            code=resp.status_code,
-            error=data.get('error', ''),
-            error_name=data.get('error_name'),
+            code=resp.status,
+            error=data.get("error", ""),
+            error_name=data.get("error_name"),
         )
 
-    delete = lambda s, *a, **k: s.request('DELETE', *a, **k)
-    get = lambda s, *a, **k: s.request('GET', *a, **k)
-    post = lambda s, *a, **k: s.request('POST', *a, **k)
-    put = lambda s, *a, **k: s.request('PUT', *a, **k)
+    def delete(self, *a, **kw):
+        return self.request("DELETE", *a, **kw)
+
+    def get(self, *a, **kw):
+        return self.request("GET", *a, **kw)
+
+    def post(self, *a, **kw):
+        return self.request("POST", *a, **kw)
+
+    def put(self, *a, **kw):
+        return self.request("PUT", *a, **kw)
 
 
 pass_api = click.make_pass_decorator(Api, ensure=True)
 
 
-@click.group(context_settings={
-    'auto_envvar_prefix': 'FREIGHT',
-})
-@click.option('--api-key', required=True)
-@click.option('--base-url', required=True)
-@click.option('--user', required=True)
-@click.option('--debug/--no-debug', default=False)
+@click.group(context_settings={"auto_envvar_prefix": "FREIGHT"})
+@click.option("--api-key", required=True)
+@click.option("--base-url", required=True)
+@click.option("--user", required=True)
+@click.option("--debug/--no-debug", default=False)
 @pass_api
 def cli(api, base_url, api_key, user, debug):
-    api.base_url = base_url.rstrip('/')
+    path = urlsplit(base_url)
+    api.base_url = "{}://{}".format(*path[:2])
+    api.path = path[2].rstrip("/")
     api.api_key = api_key
     api.user = user
     api.debug = debug
 
 
 @cli.command()
-@click.argument('app', required=True)
-@click.option('--env', '-e', default=None)
-@click.option('--ref', '-r', default=None)
-@click.option('--force', '-f', default=False, is_flag=True)
+@click.argument("app", required=True)
+@click.option("--env", "-e", default=None)
+@click.option("--ref", "-r", default=None)
+@click.option("--force", "-f", default=False, is_flag=True)
 @pass_api
 def deploy(api, app, env, ref, force):
-    params = {
-        'app': app,
-        'user': api.user,
-        'force': force,
-    }
+    params = {"app": app, "user": api.user, "force": force}
     if env:
-        params['env'] = env
+        params["env"] = env
     if ref:
-        params['ref'] = ref
-    data = api.post('/deploys/', params)
-    print('Created new Deploy: {}'.format(data.get('name', data['id'])))
+        params["ref"] = ref
+    data = api.post("/deploys/", params)
+    click.echo(f'Created new Deploy: {data.get("name", data["id"])}')
 
 
 @cli.command()
-@click.argument('task-id', required=True)
+@click.argument("task-id", required=True)
 @pass_api
 def status(api, task_id):
     task = api.parse_task_id(task_id)
-    data = api.get('/deploys/{}/{}/{}/'.format(task.app, task.env, task.number))
-    row = '{:12} {:25}'
-    print('[{app}/{env} #{number}]'.format(
-        app=data['app']['name'],
-        number=data['number'],
-        env=data['environment'],
-        id=data['id']
-    ))
-    print(row.format('Status:', data['status']))
-    print(row.format('Created:', data['dateCreated']))
-    if data['status'] in ('finished', 'failed'):
-        print(row.format('Started:', data['dateStarted']))
-        print(row.format('Finished:', data['dateFinished']))
+    data = api.get(f"/deploys/{task.app}/{task.env}/{task.number}/")
+    row = "{:12} {:25}"
+    click.echo(
+        "[{app}/{env} #{number}]".format(
+            app=data["app"]["name"],
+            number=data["number"],
+            env=data["environment"],
+            id=data["id"],
+        )
+    )
+    click.echo(row.format("Status:", data["status"]))
+    click.echo(row.format("Created:", data["dateCreated"]))
+    if data["status"] in ("finished", "failed"):
+        click.echo(row.format("Started:", data["dateStarted"]))
+        click.echo(row.format("Finished:", data["dateFinished"]))
 
 
 @cli.command()
-@click.argument('task-id', required=True)
-@click.option('--follow', '-f', is_flag=True, default=False)
-@click.option('--interval', '-i', default=0.1)
+@click.argument("task-id", required=True)
+@click.option("--follow", "-f", is_flag=True, default=False)
+@click.option("--interval", "-i", default=0.1)
 @pass_api
 def tail(api, task_id, follow, interval):
     task = api.parse_task_id(task_id)
-    data = api.get('/deploys/{}/{}/{}/log/?offset=-1&limit=1000'.format(
-        task.app, task.env, task.number
-    ))
-    offset = data['nextOffset']
-    if not data['text']:
-        sys.stdout.write('(waiting for output..)\n')
+    data = api.get(
+        f"/deploys/{task.app}/{task.env}/{task.number}/log/?offset=-1&limit=1000"
+    )
+    offset = data["nextOffset"]
+    if not data["chunks"]:
+        sys.stdout.write("(waiting for output..)\n")
     else:
-        sys.stdout.write(data['text'])
+        for chunk in data["chunks"]:
+            sys.stdout.write(chunk["text"])
     while True:
-        data = api.get('/deploys/{}/{}/{}/log/?offset={}'.format(
-            task.app, task.env, task.number, offset
-        ))
-        offset = data['nextOffset']
-        sys.stdout.write(data['text'])
+        data = api.get(
+            f"/deploys/{task.app}/{task.env}/{task.number}/log/?offset={offset}"
+        )
+        offset = data["nextOffset"]
+        for chunk in data["chunks"]:
+            sys.stdout.write(chunk["text"])
         sleep(interval)
 
 
 @cli.command()
-@click.argument('task-id', required=True)
+@click.argument("task-id", required=True)
 @pass_api
 def cancel(api, task_id):
     task = api.parse_task_id(task_id)
-    data = api.put('/deploys/{}/{}/{}/'.format(
-        task.app, task.env, task.number
-    ), {'status': 'cancelled'})
-    print('Deploy (ID = {}) was cancelled.'.format(data['id']))
+    data = api.put(
+        f"/deploys/{task.app}/{task.env}/{task.number}/", {"status": "cancelled"}
+    )
+    click.echo(f'Deploy (ID = {data["id"]}) was cancelled.')
 
 
 @cli.group()
@@ -184,69 +196,69 @@ def app():
     pass
 
 
-@app.command('list')
+@app.command("list")
 @pass_api
 def app_list(api):
-    data = api.get('/apps/')
+    data = api.get("/apps/")
     json.dump(data, sys.stdout, indent=2)
-    sys.stdout.write('\n')
+    sys.stdout.write("\n")
 
 
-@app.command('show')
-@click.argument('app', required=True)
+@app.command("show")
+@click.argument("app", required=True)
 @pass_api
 def app_show(api, app):
-    data = api.get('/apps/{}/'.format(app))
+    data = api.get(f"/apps/{app}/")
     json.dump(data, sys.stdout, indent=2)
-    sys.stdout.write('\n')
+    sys.stdout.write("\n")
 
 
-@app.command('create')
-@click.argument('name', required=True)
-@click.option('--repository', prompt=True)
-@click.option('--provider', default='shell', prompt=True)
-@click.option('--config', default='{}', prompt=True)
+@app.command("create")
+@click.argument("name", required=True)
+@click.option("--repository", prompt=True)
+@click.option("--provider", default="shell", prompt=True)
+@click.option("--config", default="{}", prompt=True)
 @pass_api
 def app_create(api, name, repository, provider, config):
     params = {
-        'name': name,
-        'repository': repository,
-        'provider': provider,
-        'provider_config': config,
+        "name": name,
+        "repository": repository,
+        "provider": provider,
+        "provider_config": config,
     }
-    api.post('/apps/', params)
-    print('Created new App: {}'.format(name))
+    api.post("/apps/", params)
+    click.echo(f"Created new App: {name}")
 
 
-@app.command('edit')
-@click.argument('app', required=True)
+@app.command("edit")
+@click.argument("app", required=True)
 @pass_api
 def app_edit(api, app):
-    data = api.get('/apps/{}/'.format(app))
+    data = api.get(f"/apps/{app}/")
     # id isn't editable
-    data.pop('id')
-    rv = click.edit(json.dumps(data, indent=2)+'\n')
+    data.pop("id")
+    rv = click.edit(json.dumps(data, indent=2) + "\n")
     if rv is None:
         return
     rv = json.loads(rv)
     params = {}
-    for k, v in rv.iteritems():
+    for k, v in rv.items():
         if isinstance(v, (list, dict)):
             params[k] = json.dumps(v)
         else:
             params[k] = v
-    api.put('/apps/{}/'.format(app), params)
-    print('App {} was updated.'.format(app))
+    api.put(f"/apps/{app}/", params)
+    click.echo(f"App {app} was updated.")
 
 
-@app.command('delete')
-@click.argument('app', required=True)
+@app.command("delete")
+@click.argument("app", required=True)
 @pass_api
 def app_delete(api, app):
-    click.confirm('Are you sure you want to delete \'%s\'?' % app, abort=True)
-    data = api.delete('/apps/{}/'.format(app))
+    click.confirm(f"Are you sure you want to delete '{app}'?", abort=True)
+    data = api.delete(f"/apps/{app}/")
     json.dump(data, sys.stdout, indent=2)
-    sys.stdout.write('\n')
+    sys.stdout.write("\n")
 
 
 @cli.group()
@@ -254,27 +266,23 @@ def webhook():
     pass
 
 
-@webhook.command('create')
-@click.option('--hook', default='github', prompt=True)
-@click.option('--action', default='deploy', prompt=True)
-@click.option('--app', prompt=True)
-@click.option('--env', prompt=True)
+@webhook.command("create")
+@click.option("--hook", default="github", prompt=True)
+@click.option("--action", default="deploy", prompt=True)
+@click.option("--app", prompt=True)
+@click.option("--env", prompt=True)
 @pass_api
 def webhook_create(api, hook, action, app, env):
     import hmac
     from hashlib import sha256
-    from urlparse import urlsplit
 
-    key = '{}/{}/{}/{}'.format(
-        hook, action, app, env,
-    )
-    base_url = '{}://{}'.format(*urlsplit(api.base_url)[:2])
-    print('{}/webhooks/{}/{}/'.format(
-        base_url,
-        key,
-        hmac.new(api.api_key.encode('utf8'), key, sha256).hexdigest(),
-    ))
+    key = f"{hook}/{action}/{app}/{env}"
+    base_url = "{}://{}".format(*urlsplit(api.base_url)[:2])
+    digest = hmac.new(
+        api.api_key.encode("utf8"), key.encode("utf8"), sha256
+    ).hexdigest()
+    click.echo(f"{base_url}/webhooks/{key}/{digest}/")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     cli()
--- setup.py
+++ setup.py
@@ -1,72 +1,25 @@
-#!/usr/bin/env python
-"""
-freight-cli
-===========
+from setuptools import find_packages, setup
 
-This is a command line interface to `Freight <https://github.com/getsentry/freight>`_.
-
-:copyright: (c) 2015 GetSentry LLC
-:license: Apache 2.0, see LICENSE for more details.
-"""
-from __future__ import absolute_import, unicode_literals
-
-import os.path
-
-from setuptools import setup, find_packages
-
-
-# Hack to prevent stupid "TypeError: 'NoneType' object is not callable" error
-# in multiprocessing/util.py _exit_function when running `python
-# setup.py test` (see
-# http://www.eby-sarna.com/pipermail/peak/2010-May/003357.html)
-for m in ('multiprocessing', 'billiard'):
-    try:
-        __import__(m)
-    except ImportError:
-        pass
-
-ROOT = os.path.realpath(os.path.join(os.path.dirname(__file__)))
-
-tests_require = [
-    'flake8>=2.1.0,<2.2.0',
-    'mock>=1.0.1,<1.1.0',
-    'pytest>=2.5.0,<2.6.0',
-    'pytest-cov>=1.6,<1.7',
-    'pytest-timeout>=0.3,<0.4',
-    'pytest-xdist>=1.9,<1.10',
-    'responses>=0.3.0,<0.4.0',
-]
-
-
-install_requires = [
-    'click>=3.3.0,<7.0.0',
-    'requests>=2.5.1,<3.0.0',
-]
+install_requires = ["click>=6.7,<7.0.0", "urllib3[secure]>=1.25,<1.26"]
 
 setup(
-    name='freight-cli',
-    version='0.0.0',
-    author='David Cramer',
-    author_email='dcramer@gmail.com',
-    url='https://github.com/getsentry/freight-cli',
-    description='A command line interface to Freight',
-    long_description=open('README.md').read(),
-    packages=find_packages(exclude=['tests']),
+    name="freight-cli",
+    version="0.0.0",
+    author="David Cramer",
+    author_email="dcramer@gmail.com",
+    url="https://github.com/getsentry/freight-cli",
+    description="A command line interface to Freight",
+    long_description=open("README.md").read(),
+    packages=find_packages(),
     zip_safe=False,
     install_requires=install_requires,
-    extras_require={
-        'test': tests_require,
-    },
-    entry_points='''
-        [console_scripts]
-        freight=freight_cli:cli
-    ''',
-    license='Apache 2.0',
+    entry_points={"console_scripts": ["freight=freight_cli:cli"]},
+    license="Apache 2.0",
     include_package_data=True,
     classifiers=[
-        'Intended Audience :: Developers',
-        'Intended Audience :: System Administrators',
-        'Operating System :: OS Independent',
-        'Topic :: Software Development'
+        "Intended Audience :: Developers",
+        "Intended Audience :: System Administrators",
+        "Operating System :: OS Independent",
+        "Topic :: Software Development",
     ],
 )
